6452ac6ffe75125457d04c1f86ab8d01442bf860
yunsjak <yunsujung0819@naver.com>
2025-06-26 16:11:34 +0900

    상세페이지 생성(th:href 속성, Service 사용, 예외처리, @PathVariable) + url prefix 활용

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,19 +5,33 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+// @RequestMapping을 이용하여 프리픽스(prefix 접두사) 지정
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
-	private final QuestionRepository questionRepository;
+	// Controller에 있는 Repository 기능을 모두 Service에게 전달
+	private final QuestionService questionService;
 
-	@GetMapping("/question/list")
+	// 질문 목록
+	@GetMapping("/list")
 	public String list(Model model) {
-		List<Question> ql = this.questionRepository.findAll();
+		List<Question> ql = this.questionService.getList();
 		model.addAttribute("questionlist", ql);
 		return "question_list";
+	}
+
+	// 상세 조회
+	@GetMapping(value = "/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question qd = this.questionService.getQuestion(id);
+		model.addAttribute("question", qd);
+		return "question_detail";
 	}
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,8 +1,11 @@
 package com.mysite.sbb.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DateNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,5 +17,14 @@ public class QuestionService {
 
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
+	}
+
+	public Question getQuestion(Integer id) {
+		Optional<Question> qo = this.questionRepository.findById(id);
+		if (qo.isPresent()) {
+			return qo.get();
+		} else {
+			throw new DateNotFoundException("question not found");
+		}
 	}
 }
