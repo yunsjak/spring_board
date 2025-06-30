5f9fe9a782781e0b3223182ef901c35c2b826081
yunsjak <yunsujung0819@naver.com>
2025-06-30 17:14:07 +0900

    Form 클래스 활용 - 답변 등록 기능에 Form 적용(유효성 검사), 공통 오류메세지 template 생성

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,14 +2,15 @@ package com.mysite.sbb.answer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/answer")
@@ -21,11 +22,23 @@ public class AnswerController {
 	private final QuestionService questionservice;
 	private final AnswerService answerService;
 
+	/*
+	 * @PostMapping("/create/{id}") public String create(Model
+	 * model, @PathVariable("id") Integer id, @RequestParam(value = "content")
+	 * String content) { Question question = this.questionservice.getQuestion(id);
+	 * // todo: 답변 저장 this.answerService.create(question, content); return
+	 * String.format("redirect:/question/detail/%s", id); }
+	 */
+
 	@PostMapping("/create/{id}")
-	public String create(Model model, @PathVariable("id") Integer id, @RequestParam(value = "content") String content) {
+	public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm,
+			BindingResult bindingResult) {
 		Question question = this.questionservice.getQuestion(id);
-		// todo: 답변 저장
-		this.answerService.create(question, content);
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("question", question);
+			return "question_detail";
+		}
+		this.answerService.create(question, answerForm.getContent());
 		return String.format("redirect:/question/detail/%s", id);
 	}
 }
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mysite.sbb.answer.AnswerForm;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 // @RequestMapping을 이용하여 프리픽스(prefix 접두사) 지정
@@ -31,22 +34,24 @@ public class QuestionController {
 
 	// 상세 조회
 	@GetMapping(value = "/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		Question qd = this.questionService.getQuestion(id);
 		model.addAttribute("question", qd);
 		return "question_detail";
 	}
 
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 
 	@PostMapping("/create")
-	public String questionCreate(@RequestParam(value = "subject") String subject,
-			@RequestParam(value = "content") String content) {
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
 		// TODO 질문 저장 (Service 처리)
-		this.questionService.create(subject, content);
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
 	}
 }
