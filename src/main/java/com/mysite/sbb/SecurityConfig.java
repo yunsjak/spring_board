e6548c25290ebe81df59551ff261eb1d13e0fcb1
yunsjak <yunsujung0819@naver.com>
2025-07-02 14:10:09 +0900

    게시물에 번호 지정, 답변 개수 표시, 스프링 Security  설정

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll()) // 모든 페이지 접근 가능
+
+				// h2-console을 보안에서 허용
+				.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+				.headers((headers) -> headers.addHeaderWriter(
+						new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+		return http.build();
+	}
+}
